b3db9c651443477c9ba298299bbeb248d2d93962
John Grey <johngrey4296@gmail.com>
2025-12-03 19:57:15 +0000

    [add]: stub create cmd

---
+++ langs-util/csound/lang-{{cookiecutter.lang_name}}/create.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# create.bash -*- mode: sh -*-
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+# shellcheck disable=SC1091
+source "$POLY_SRC/lib/lib-util.bash"
+if [[ -e "$POLYGLOT_ROOT/.tasks/task-util.bash" ]]; then
+   # shellcheck disable=SC1091
+    source "$POLYGLOT_ROOT/.tasks/task-util.bash"
+fi
+
+PGLANG="csound"
+TARGET="$POLYGLOT_SRC/_utilities/csd"
+
+tdot "[$PGLANG]" "creating a workspace member in $TARGET"
+
+if [[ -d "$TARGET" ]]; then
+    exit 0
+fi
+
+dname=$(dirname "${BASH_SOURCE[0]}")
+cp -r "$dname/_base" "$TARGET"
