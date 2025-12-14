b39e916281edc08cc0bc0d429634e1a67f66a116
John Grey <johngrey4296@gmail.com>
2025-12-14 00:00:48 +0000

    [add]: stub tools

---
+++ langs-util/clips/tool-{{cookiecutter.tool_name}}/run.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+# shellcheck disable=SC1091
+source "$POLY_SRC/lib/lib-util.bash"
+if [[ -e "$POLYGLOT_ROOT/.tasks/task-util.bash" ]]; then
+    # shellcheck disable=SC1091
+    source "$POLYGLOT_ROOT/.tasks/task-util.bash"
+fi
+
+shift
+DEFAULT_FILE="main.clips"
+target="_ai_and_logic/clips"
+file="${1:-$DEFAULT_FILE}"
+shift 1
+
+tdot "[clips]" "Running: $target/$file"
+clips -f "$POLYGLOT_SRC/$target/$file" "${@:-}"
---
+++ langs-util/fact/lang-{{cookiecutter.lang_name}}/run.bash
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+# A Stub language command.
+# Place in $root/.tasks/lang-{name}/{cmd}.bash
+# and chmod +x it.
+set -o nounset
+set -o pipefail
+
+DEFAULT_FILE="main.fact"
+
+# shellcheck disable=SC1091
+source "$POLY_SRC/lib/lib-util.bash"
+if [[ -e "$POLYGLOT_ROOT/.tasks/task-util.bash" ]]; then
+    # shellcheck disable=SC1091
+    source "$POLYGLOT_ROOT/.tasks/task-util.bash"
+fi
+
+function print-help () {
+    case "${@: -1}" in
+        -h|--help) ;;
+        *) if [[ "$#" -gt 0 ]]; then
+               return
+           fi
+           ;;
+    esac
+    echo -e "
+usage: polyglot lang fact run [-h] [package] [filename|--] [args...]
+
+run a FaCT description logic spec file
+
+positional arguments:
+package   : The subdir of $POLYGLOT_SRC to target
+filename  : the file of the package to use
+args      : arguments to pass to the command
+
+options:
+-h, --help    : show this help message and exit
+
+"
+    exit "${PRINTED_HELP:-2}"
+}
+
+function handle-result () {
+    result="$1"
+    shift
+
+    case "$result" in
+        0) exit 0 ;;
+        1) fail "Command Failed" ;;
+        *) fail "Unknown result code: $result"
+    esac
+}
+
+function run-program () {
+    file="$1"
+    shift
+    _ARGS=(
+        "-T:10" # timeout in ten seconds
+        "$@"
+        )
+
+    mkdir -p "$POLYGLOT_TEMP/fact"
+    tdot "[FaCT]" "Running: $file"
+    fail "TODO"
+}
+
+function main () {
+    print-help "$@"
+    tdot "[FaCT]" "Parsing Args"
+    shift
+    case "$1" in
+        --) file="$DEFAULT_FILE" ;;
+        *)  file="$2" ;;
+    esac
+    shift 2
+    _ARGS=("$@")
+    check-target "_ai_and_logic/fact" "$file"
+    run-program "$file" "${_ARGS[@]}"
+    handle-result "$?"
+    exit "$?"
+}
+
+main "$@"
